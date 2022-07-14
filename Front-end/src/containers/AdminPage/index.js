e912125c4a05771cf065c99ae7fd10b92c996e39
phuc.vu <van003v@prestigein.com>
2022-07-14 09:00:18 +0700

    update user

--- Front-end/src/containers/AdminPage/index.js
+++ Front-end/src/containers/AdminPage/index.js
@@ -244,7 +244,7 @@ function AdminPage(props) {
                 <Route key={"category"} path={`${match.path}/category`} exact={true} component={CategoryList} />
                 <Route key={"product"} path={`${match.path}/product`} exact={true} component={ProductList} />
                 <Route key={"transaction"} path={`${match.path}/transaction`} exact={true} component={TransactionList} />
-				<Route key={"user"} path={`${match.path}/user`} exact={true} component={TransactionList} />
+				        <Route key={"user"} path={`${match.path}/user`} exact={true} component={TransactionList} />
                 <Route key={"slide"} path={`${match.path}/slide`} exact={true} component={SlideList} />
                 <Route key={"dashboard"} path={""} exact={true} component={Dashboard} />
                 <Route>
